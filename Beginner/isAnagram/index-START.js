5e1711ad99e9c6a70e654bacb3d48f65839c5b28
KatKava <katryn.kav97@gmail.com>
2021-06-05 20:59:19 +0100

    anagram algorithm tests passed

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -6,10 +6,42 @@ don't. E.g
 */
 
 
-
+//Direct comparison 
 function isAnagram(stringA, stringB) {
     // Code goes here
+    const sanitizeString = function (str) {
+        return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join(''); 
+    }
+    return sanitizeString(stringA) == sanitizeString(stringB)
 }
 
+//character Map Comparison 
+function isAnagram(stringA, stringB) {
 
+function createCharMap(text) {
+
+    let charMap = {}
+    
+    for(let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+        charMap[char]++
+    }else{
+        charMap[char] = 1
+    }
+    }
+    return charMap
+} 
+     if(stringA.length === stringB.length) {
+         let stringAMap = createCharMap(stringA) 
+         let stringBMap = createCharMap(stringB) 
+         for(let char in stringAMap) {
+             if(stringAMap[char] !== stringBMap[char]) {
+                 return false
+             }
+         }
+         return true
+     } else {
+         return false
+     }
+} 
 module.exports = isAnagram
--- Beginner/searchReplace/index-START.js
+++ Beginner/searchReplace/index-START.js
@@ -16,3 +16,4 @@ function searchReplace(str, word, newWord) {
  }
  return str.replace(regex, newWord)
 }
+
