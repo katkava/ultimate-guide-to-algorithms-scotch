246d9f8099ba2191fc6c41502cc46e002e3522f0
KatKava <katryn.kav97@gmail.com>
2021-06-05 19:52:09 +0100

    palindrome algorithm tests passed

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -4,10 +4,32 @@ e.g palindromeChecker('racecar') // will return true
 */
 
 
-
+// 1) Intuitive Approach (direct approach to solving)
 
 function palindromeChecker(text) {
-    v// Code goes here
+    const reversedText = text.toLowerCase().split("").reverse().join("")
+    return text === reversedText
+}
+
+// 2) looping through and comparing characters 
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {return letter === charArray[charArray.length - index -1]; 
+    })
+    return result 
+}
+
+//looping through optimized 
+
+function palindromeChecker(text) {
+ let textLen = text.length;
+ for(var i =0; i < textLen/2; i++) {
+     if (text[i] !== text[textLen -1 -i]) {
+         return false
+     }
+ }
+ return true; 
 }
 
 
