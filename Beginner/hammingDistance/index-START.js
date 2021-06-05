bf5981cb3c69b8415183399ce3b5cec065564087
KatKava <katryn.kav97@gmail.com>
2021-06-05 20:08:18 +0100

    hamming distance test passed

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -5,10 +5,24 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 
-
+// using a for-loop 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    let result = 0
+
+    if (stringA.length == stringB.length) {
+
+        for(let i = 0; i < stringA.length; i++) {
+            if (stringA[i].toLowerCase() != stringB[i].toLowerCase()) {
+                result++
+            }
+        }
+        return result
+
+    } else {
+        throw new Error('Strings do not have equal length ')
+    }
 }
+
 
 
 
