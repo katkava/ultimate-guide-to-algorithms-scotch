8000d7ff2988815034241357004574c1fe77a963
KatKava <katryn.kav97@gmail.com>
2021-06-08 20:02:11 +0100

    falsy Bouncer algorithm

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -8,7 +8,23 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 
 function falsyBouncer(array) {
     // Code goes here
+    let result = []
+    //loop through with each array value 
+    for(value of array){
+        //push into result if truthy
+        if(value){
+            result.push(value)
+        }
+    }
+    return result 
 }
 
+//using filter 
+
+function falsyBouncer(array){
+    return array.filter((value) => {
+        return Boolean(value)
+    })
+}
 
 module.exports = falsyBouncer
