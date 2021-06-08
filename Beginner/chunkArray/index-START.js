f2f0dfd0f528b754fb24d76582f4a82eeed34257
KatKava <katryn.kav97@gmail.com>
2021-06-08 19:32:56 +0100

    chunky array algorithm test passed

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,9 +4,48 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+// looping through the array 
+
 function chunkArray(array, size) {
     // Code goes here
+    let result = []
+    for (value of array) {
+        let lastArray = result[result.length-1]
+        if(!lastArray || lastArray.length == size){
+            result.push([value])
+        }else{
+            lastArray.push(value)
+        }
+    }
+    return result
 }
 
+// looping through the number of chunks 
+function chunkArray(array, size) {
+    let result = []
+    let arrayCopy = [...array]
+    while(arrayCopy.length>0){
+        result.push(arrayCopy.splice(0, size))
+    }
+    return result 
+}
+
+//slice 
+function chunkArray(array, size) {
+    let result = []
+    for(let i = 0; i<array.length; i+=size) {
+        let chunk = array.slice(i, i+size)
+        result.push(chunk)
+    }
+    return result 
+}
+
+//recursion 
+function chunkarray(array, size) {
+    if(array.length <= size){
+        return [array]
+    }
+    return [array.slice(0, size), ...chunkArray(array.slice(size), size)]
+}
 
 module.exports = chunkArray
